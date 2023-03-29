d618b1f52c500607f120bd604081141f446a5897
GoneSahlin <zach@sahlins.net>
2023-03-28 21:24:47 -0700

    added normalization and graphing

--- model/window_generator.py
+++ model/window_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 class WindowGenerator():
@@ -49,6 +50,39 @@ class WindowGenerator():
         labels.set_shape([None, self.input_width, None])
 
         return inputs, labels
+    
+    def plot(self, model=None, plot_col='Price', max_subplots=3):
+        inputs, labels = self.example
+        plt.figure(figsize=(12, 8))
+        plot_col_index = self.column_indices[plot_col]
+        max_n = min(max_subplots, len(inputs))
+        for n in range(max_n):
+            plt.subplot(max_n, 1, n+1)
+            plt.ylabel(f'{plot_col} [normed]')
+            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                     label='Inputs', marker='.', zorder=-10)
+            
+            if self.label_columns:
+                label_col_index = self.label_columns_indices.get(plot_col, None)
+            else:
+                label_col_index = plot_col_index
+            
+            if label_col_index is None:
+                continue
+
+            plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
+            
+            if model is not None:
+                predictions = model(inputs)
+                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                            marker='X', edgecolors='k', label='Predictions',
+                            c='#ff7f0e', s=64)
+
+            if n == 0:
+                plt.legend()
+
+            plt.xlabel('Time [days]')
 
     def make_dataset(self, data):
         data = np.array(data, dtype=np.float32)
